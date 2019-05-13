6a311372f5d3f31467f6729978152949e0d4406d
Calin Culianu <calin.culianu@gmail.com>
2019-05-13 16:10:17 +0300

    udev rules installer, follow-up 3: Tiny nits in terms of wording,etc

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -732,7 +732,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         tools_menu.addAction(_("Installed &Plugins") + "...", self.external_plugins_dialog, QKeySequence("Ctrl+P"))
         if sys.platform == 'linux':
             tools_menu.addSeparator()
-            tools_menu.addAction(_("&Hardware wallet support"), self.hardware_wallet_support)
+            tools_menu.addAction(_("&Hardware wallet support..."), self.hardware_wallet_support)
         tools_menu.addSeparator()
         tools_menu.addAction(_("&Sign/verify message") + "...", self.sign_verify_message)
         tools_menu.addAction(_("&Encrypt/decrypt message") + "...", self.encrypt_message)
