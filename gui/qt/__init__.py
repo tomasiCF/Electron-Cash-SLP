fa7260e3e3467781e24dcdbbcc0deeeda65970d4
James Cramer <jcramer@simpleledger.io>
2019-05-29 15:36:22 -0400

    fix high dpi scaling for some systems

--- gui/qt/__init__.py
+++ gui/qt/__init__.py
@@ -33,7 +33,10 @@ except Exception:
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+try:
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+except:
+    pass
 
 from electroncash.i18n import _, set_language
 from electroncash.plugins import run_hook
