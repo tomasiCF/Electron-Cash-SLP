fa88a7471fc3d1f40639d360bf1dd73ddb0eb8a9
Calin Culianu <calin.culianu@gmail.com>
2019-06-07 00:40:39 +0300

    Nit: rename seal -> seal.svg in slp_history_list

--- gui/qt/slp_history_list.py
+++ gui/qt/slp_history_list.py
@@ -245,7 +245,7 @@ class HistoryList(MyTreeWidget):
             if child_tx:
                 menu.addAction(_("Child pays for parent"), lambda: self.parent.cpfp(tx, child_tx))
         if pr_key:
-            menu.addAction(QIcon(":icons/seal"), _("View invoice"), lambda: self.parent.show_invoice(pr_key))
+            menu.addAction(QIcon(":icons/seal.svg"), _("View invoice"), lambda: self.parent.show_invoice(pr_key))
         if tx_URL:
             menu.addAction(_("View on block explorer"), lambda: webbrowser.open(tx_URL))
         menu.exec_(self.viewport().mapToGlobal(position))
