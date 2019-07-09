63570405fe1f37d61a86aef8aa50495a37a6ba60
Mark B Lundeberg <36528214+markblundeberg@users.noreply.github.com>
2019-07-08 22:24:26 -0700

    Liberalize transaction deserializer and restrict merkle node checker. (#1506)
    * Liberalize transaction deserializer and restrict merkle node checker.
    
    In #1457, the transaction deserializer was restricted for the sake of the
    merkle node checker, however I realized that the merkle node checker can
    do strict checks on its own, and the transaction deserializer can be
    liberal which may be useful for smart contracts (e.g., crowd funds using
    ANYONECANPAY that start out with 0 inputs).
    
    Reviewers: please examine for runtime errors since this code runs
    **extremely** infrequently: the txin, and txout, lines only get passed by
    1 in a billion transactions.
    
    * verifier: catch explicit exceptions in `_raise_if_valid_tx`
    
    Note the big assumption here that tx deserialization can only throw
    `SerializationError` ... you will pretty much immediately get exceptions
    thrown in trying to restore a big wallet, if this is not the case.
    
    * Rudely commit over Mark's PR because I'm OCD
    
    I don't trust the deserialization code not to raise other than
    Serialization error.  While it looks like it can only raise that -- who
    knows.
    
    Also I like customary groupings by 3 of thousands.. :P

--- lib/transaction.py
+++ lib/transaction.py
@@ -394,10 +394,8 @@ def deserialize(raw):
     start = vds.read_cursor
     d['version'] = vds.read_int32()
     n_vin = vds.read_compact_size()
-   # assert n_vin != 0  ## This unnecessary check has been removed so as to enable tests/test_slp_consensus to work with self-contained fake transaction DAGs.
     d['inputs'] = [parse_input(vds) for i in range(n_vin)]
     n_vout = vds.read_compact_size()
-    assert n_vout != 0
     d['outputs'] = [parse_output(vds, i) for i in range(n_vout)]
     d['lockTime'] = vds.read_uint32()
     if vds.can_read_more():
