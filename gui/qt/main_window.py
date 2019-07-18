14ad8b87d5e1d6344fd9ca46b90a08c6e2fb3279
Calin Culianu <calin.culianu@gmail.com>
2019-07-18 23:22:45 +0300

    Qt: UI nit, make Address Dialog show on top of top_level_window
    This is required by up-coming Cash Accounts integration.  I cross picked
    this commit from EC Regular for that purpose.

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -1063,7 +1063,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
         return self.create_list_tab(l)
 
     def show_address(self, addr, *, parent=None):
-        parent = parent or self
+        parent = parent or self.top_level_window()
         from . import address_dialog
         d = address_dialog.AddressDialog(self,  addr, windowParent=parent)
         d.exec_()
