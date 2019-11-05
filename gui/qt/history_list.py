cf8b42df34ac1d219f62f519afdd0147f1216e77
Calin Culianu <calin.culianu@gmail.com>
2019-11-06 00:38:42 +0200

    Qt history tab: Added workaround for persitent 'Unknown' balances
    This can happen if the input's script is somewhat complex and the
    pruned_txo_values cache has to try and download prevouts and figure out
    the addresses in question.
    
    The fact that pruned_txo_values has stuff in it causes the
    get_wallet_delta function to return "None", which makes the GUI render
    "Unknown" in the balance column.
    
    This situation would eventually correct itself after the wallet settles.
    
    The code we added was to schedule a GUI refresh sometime after the last
    tx was verified and after the wallet settled, to clearn the "Unknown"
    balances from the history tab.
    
    This is a follow-up to PR #1620.
    
    These persistent 'Unknown' balances are caused by the code added in that
    PR, and this workaround is now needed as a result.

--- gui/qt/history_list.py
+++ gui/qt/history_list.py
@@ -59,6 +59,11 @@ class HistoryList(MyTreeWidget):
         self.invoiceIcon = QIcon(":icons/seal")
         self._item_cache = Weak.ValueDictionary()
 
+        self.has_unknown_balances = False
+
+    def clean_up(self):
+        self.cleaned_up = True
+
     def refresh_headers(self):
         headers = ['', '', _('Date'), _('Description') , _('Amount'), _('Balance')]
         fx = self.parent.fx
@@ -108,11 +113,19 @@ class HistoryList(MyTreeWidget):
         current_tx = sels[0].data(0, Qt.UserRole) if sels else None
         del sels #  make sure not to hold stale ref to C++ list of items which will be deleted in clear() call below
         self.clear()
+        self.has_unknown_balances = False
         fx = self.parent.fx
         if fx: fx.history_used_spot = False
         for h_item in h:
             tx_hash, height, conf, timestamp, value, balance = h_item
             label = self.wallet.get_label(tx_hash)
+            if value is None or balance is None:
+                # Workaround to the fact that sometimes the wallet doesn't
+                # know the actual balance for history items while it's
+                # downloading history, and we want to flag that situation
+                # and redraw the GUI sometime later when it finishes updating.
+                # This flag is checked in main_window.py, TxUpadteMgr class.
+                self.has_unknown_balances = True
             status, status_str = self.wallet.get_tx_status(tx_hash, height, conf, timestamp)
             has_invoice = self.wallet.invoices.paid.get(tx_hash)
             icon = self._get_icon_for_status(status)
